234d4a9384c406d3df079036aef21df436a50715
singra <afcardenal27@gmail.com>
2020-07-13 12:39:46 +0200

    Test para comprobar double con tolerancia en valor y porcentaje

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -84,5 +84,12 @@ namespace Loans.Tests
             Assert.That(x, Is.Not.SameAs(y));
 
         }
+        [Test]
+        public void Double()
+        {
+            double a = 1.0 / 3.0;
+            Assert.That(a, Is.EqualTo(0.33).Within(0.004));
+            Assert.That(a, Is.EqualTo(0.33).Within(10).Percent);
+        }
     }
 }
